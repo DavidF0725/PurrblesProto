7f7ca3d5a3919f30deebf1a391e3246cb8325c2a
David Favier <ac100784@avcol.school.nz>
2021-08-24 01:23:25 +1200

    I have completed the crouch mechanic for the character. Next time I will focus on finishing the falling animation and will make improvements on the Main camera of the character in unity and allow a smooth camera follow.

--- Assets/Fox.cs
+++ Assets/Fox.cs
@@ -3,17 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Fox : MonoBehaviour
-{
-    //Public Fields
-    public float speed = 1;
+{   
+    
+    Rigidbody2D rb;
+    Animator anim;
+    [SerializeField] Collider2D standingCollider;
+    [SerializeField] Transform groundCheckColl;
+    [SerializeField] Transform ceilingCheckColl;
+    [SerializeField] LayerMask groundLayer;
 
     //Inspector Variables
-     Rigidbody2D rb;
-    Animator anim;
+    const float groundCheckRadius = 0.2f;
+    const float ceilingCheckRadius = 0.2f;
+    [SerializeField] float speed = 2;
+    [SerializeField] float jumpPower = 500;
     float horizontalValue;
     float runSpeedModifier = 2f;
+    float crouchSpeedModifier = 0.5f;
+
+    [SerializeField] bool isGrounded = false;
     bool isRunning = false;
     bool facingRight = true;
+    bool jump = false;
+    bool crouchPressed = false;
 
     void Awake()
     {
@@ -32,24 +44,86 @@ public class Fox : MonoBehaviour
         //If the user releases left shift, stops running
         if (Input.GetKeyUp(KeyCode.LeftShift))
             isRunning = false;
+
+        //If we press jump
+        if (Input.GetButtonDown("Jump"))
+            jump = true;
+        //If we release space bar - fall
+        else if (Input.GetButtonUp("Jump"))
+            jump = false;
+
+        //If we press crouch
+        if (Input.GetButtonDown("Crouch"))
+            crouchPressed = true;
+        //If we release crouch - stand
+        else if (Input.GetButtonUp("Crouch"))
+            crouchPressed = false;
     }
 
 
     //Every time the user presses a key
     void FixedUpdate()
     {
-        Move(horizontalValue);
+        GroundCheck();
+        Move(horizontalValue, jump, crouchPressed);
+    }
+
+    void GroundCheck()
+    {
+        isGrounded = false;
+
+        //Check if the player is touching the ground 
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheckColl.position, groundCheckRadius, groundLayer);
+        if (colliders.Length > 0)
+            isGrounded = true;
     }
 
 
-    void Move(float direction)
+    void Move(float direction, bool jumpFlag, bool crouchFlag)
     {
-        //Set value of x and direction and speed
-        float xValue = direction * speed * 100 * Time.deltaTime;
+        #region Jump and Crouch
 
-        //If we are running, multiply with the running modifier 
+        //If we are crouch and then disable crouching
+        //Check above player head for collisions
+        //If there are any - stay crouched | if not, uncrouch
+        if(!crouchFlag)
+        {
+            if (Physics2D.OverlapCircle(ceilingCheckColl.position, ceilingCheckRadius, groundLayer))
+                crouchFlag = true;
+        }
+
+
+
+        //If crouch - standingCollider = false + enable crouch anim
+        //Reduce the x velocity
+        //If uncrouch - standingCollider = true + disable crouch anim
+        if(isGrounded)
+        {
+            standingCollider.enabled = !crouchFlag;
+            //Jump
+            if (jumpFlag)
+            {
+                isGrounded = false;
+                jumpFlag = false;
+                //Jump force implemented
+                rb.AddForce(new Vector2(0f, jumpPower));
+            }
+        }
+
+        anim.SetBool("Crouch", crouchFlag);
+
+        #endregion
+
+        #region Move and Run
+        //Set value of x and direction and speed
+        float xValue = direction * speed * 100 * Time.fixedDeltaTime;
+
+        //If we are running, multiply with the running modifier (higher)
         if (isRunning)
             xValue *= runSpeedModifier;
+        //If we are running, multiply with the running modifier (higher)
+        if (crouchFlag)
+            xValue *= crouchSpeedModifier;
         //Set Vector2 for the velocity
         Vector2 targetVelocity = new Vector2(xValue, rb.velocity.y);
         //Set the player's velocity
@@ -72,5 +146,6 @@ public class Fox : MonoBehaviour
         //Set the float xVelocity according to the x value 
         // of the RigidBody2D velocity
         anim.SetFloat("xVelocity", Mathf.Abs(rb.velocity.x));
+        #endregion
     }
 }
